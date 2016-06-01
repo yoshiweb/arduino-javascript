6d16ead50fb6fe12426b1f1548888e51bca7f11c
yoshiweb <yoshiweb@gmail.com>
2016-06-01 21:05:21 +0900

    socket.ioで メッセージの送信／受信

---
+++ socket.js
@@ -0,0 +1,54 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+// var fs = require('fs');
+
+
+// httpサーバー起動
+app.listen(3000);
+
+
+
+/**
+ * HTTPリクエストには文字列を返す
+ */
+function handler (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}
+
+// ソケット接続
+io.on('connection', function (socket) {
+  var id = socket.id;
+
+  console.log("新規接続", id );
+
+
+  // 接続してきたユーザーにメッセージを送信
+  socket.emit('success', { id: id });
+
+  // すでに接続してる人に配信
+  socket.broadcast.emit('connected', { id: id });
+
+
+  // 接続してきたユーザーからのメッセージを待機
+  socket.on('message', function (data) {
+
+    // 接続してきたユーザーから送られてきたデータ
+    console.log('message', data);
+
+    // 送ってきた人以外に配信
+    socket.broadcast.emit('broadcast', data);
+
+  });
+
+
+  // 接続解除を検知
+  socket.on('disconnect', function (data) {
+
+      console.log("接続解除", id, data);
+
+    // 他の人が接続解除したことを配信
+    socket.broadcast.emit('disconnected', { id: id });
+  });
+
+});
