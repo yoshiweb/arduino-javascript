ba890ceba0ea4bd43d9fdeddab86d95db18b0378
yoshiweb <yoshiweb@gmail.com>
2016-06-01 21:01:00 +0900

    node.js からLチカさせる
    - ArduinoをUSBで接続
    - Arduino IDEを起動
    - ファイル→スケッチの例→Firmata→StandardFirmata
    - マイコンボードに書き込む
    - blink.js 作成
    - $ node blink.js

---
+++ blink.js
@@ -0,0 +1,14 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  
+  // Create an Led on pin 13
+  var led = new five.Led(13);
+
+  // Blink every half second
+  led.blink(500);
+});
+
+// 参考
+// https://github.com/rwaldron/johnny-five
